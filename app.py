a1da4436b09c0b75838f939f0c6ea7128128356b
Ronald Rodriguez Castro <bb.rfrodriguezc@gmail.com>
2024-07-27 09:07:07 -0500

    frontend
    frontend

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def crear_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-@app.route('/lista')
-def lista_ingredientes():
-    return render_template('lista_ingredientes.html')
+    @app.route('/lista')
+    def lista_ingredientes():
+        return render_template('lista_ingredientes.html')
+    return app
 
 if __name__ == '__main__':
+    app = crear_app()
     app.run(port=5000)
